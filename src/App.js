35ce76de4c163fbf15ad8cab92145658e709094b
Tony Hopkins <syiehya50@gmail.com>
2024-02-18 23:08:39 +0200

    first commit: added all files to the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import Accordion from "./components/accordion"
+import RandomColor from "./components/random-color"
+import StarRating from "./components/star-rating"
+import ImageSlider from "./components/image-slider"
+// API key for pexels.com sCYE656aez48apGkQNUPojvq3qM1qMdYf75SEJ3BP3qp5ErsR3iitXz1
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Accordion />
+      <RandomColor /> */}
+      {/* <StarRating /> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
