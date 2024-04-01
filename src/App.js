84db538138e79d6e4712cfedbc3bd7215c42b5a9
Tony Hopkins <syiehya50@gmail.com>
2024-04-01 15:49:05 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -14,25 +14,23 @@ import TabTest from "./components/custom-tabs/tab-test"
 import ModalTest from "./components/custom-moda-popup/modal-test"
 import GithubProfileFinder from "./components/github-profile-finder"
 import SearchAutoComplete from "./components/search-autocomplete-with-api"
-import TreeViewTest from "./components/tree-view-test"
 
 function App() {
   return (
     <div className="App">
-      <TreeViewTest menus={menus} />
-      {/* <SearchAutoComplete /> */}
-      {/* <GithubProfileFinder /> */}
-      {/* <ModalTest /> */}
-      {/* <TabTest /> */}
-      {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
-      {/* <LightDarkMode /> */}
-      {/* <QrCodeGenerator /> */}
-      {/* <TreeView menus={menus} /> */}
-      {/* <LoadMoreData /> */}
-      {/* <Accordion /> */}
-      {/* <RandomColor /> */}
-      {/* <StarRating /> */}
-      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
+      <SearchAutoComplete />
+      <GithubProfileFinder />
+      <ModalTest />
+      <TabTest />
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+      <LightDarkMode />
+      <QrCodeGenerator />
+      <TreeView menus={menus} />
+      <LoadMoreData />
+      <Accordion />
+      <RandomColor />
+      <StarRating />
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
     </div>
   )
 }
